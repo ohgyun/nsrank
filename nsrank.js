bada3072aa8ea028da63f837174e9fd9a67acba7
Ohgyun Ahn <ohgyun@gmail.com>
2013-10-10 15:21:41 +0900

    http 요청보내기

--- nsrank.js
+++ nsrank.js
@@ -1,7 +1,32 @@
-// 상대 경로로 다른 모듈을 가져옵니다.
 var urlReader = require('./urlReader');
+var http = require('http');
 
 urlReader.read(function (url) {
-    // 가져온 URL로 출력하기
+
     console.log(url);
+
+    // http.request()로 보낼 수 있지만,
+    // url을 host, path 등으로 나눠서 보내야해요.
+    var req = http.request({
+        host: 'www.naver.com',
+        path: '/',
+        port: 80
+    });
+
+    // 응답이 왔을 오면 아래 이벤트가 발생합니다.
+    req.on('response', function (res) {
+        var result = '';
+        res.setEncoding('utf8');
+
+        res.on('data', function (chunk) {
+            result += chunk;
+        });
+
+        res.on('end', function () {
+            console.log(result);
+        });
+    });
+
+    req.end();
+    
 });
