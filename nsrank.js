38ced5f487dbb31bd2db8a0464ded9a199e9605d
Ohgyun Ahn <ohgyun@gmail.com>
2013-10-10 15:30:07 +0900

    랭크를 가져와서 파싱하고 모듈로 export 하기

--- nsrank.js
+++ nsrank.js
@@ -2,30 +2,41 @@ var urlReader = require('./urlReader');
 var http = require('http');
 var url = require('url');
 
-urlReader.read(function (urlText) {
+function getRanks(callback) {
+    urlReader.read(function (urlText) {
 
-    var urlData = url.parse(urlText);
+        var urlData = url.parse(urlText);
 
-    var req = http.request({
-        host: urlData.host,
-        path: urlData.path,
-        method: 'GET',
-        port: 80
-    });
-
-    req.on('response', function (res) {
-        var result = '';
-        res.setEncoding('utf8');
-
-        res.on('data', function (chunk) {
-            result += chunk;
+        var req = http.request({
+            host: urlData.host,
+            path: urlData.path,
+            method: 'GET',
+            port: 80
         });
 
-        res.on('end', function () {
-            console.log(result);
-        });
-    });
+        req.on('response', function (res) {
+            var result = '';
+            res.setEncoding('utf8');
 
-    req.end();
-    
-});
+            res.on('data', function (chunk) {
+                result += chunk;
+            });
+
+            res.on('end', function () {
+                var ranks = [];
+                var regex = /<K>(.+?)<\/K>/g;
+
+                while (regex.test(result)) {
+                    ranks.push(RegExp.$1);
+                }
+
+                callback(ranks);
+            });
+        });
+
+        req.end();
+
+    });
+}
+
+exports.get = getRanks;
