195780e3b6831b8dc416c7c70e66b10e47fb01aa
Ohgyun Ahn <ohgyun@gmail.com>
2013-10-10 15:35:15 +0900

    http 모듈을 restler로 변경

--- nsrank.js
+++ nsrank.js
@@ -1,40 +1,19 @@
 var urlReader = require('./urlReader');
-var http = require('http');
-var url = require('url');
+var restler = require('restler');
 
 function getRanks(callback) {
     urlReader.read(function (urlText) {
 
-        var urlData = url.parse(urlText);
+        restler.get(urlText).on('complete', function (result) {
+            var ranks = [];
+            var regex = /<K>(.+?)<\/K>/g;
 
-        var req = http.request({
-            host: urlData.host,
-            path: urlData.path,
-            method: 'GET',
-            port: 80
+            while (regex.test(result)) {
+                ranks.push(RegExp.$1);
+            }
+
+            callback(ranks);
         });
-
-        req.on('response', function (res) {
-            var result = '';
-            res.setEncoding('utf8');
-
-            res.on('data', function (chunk) {
-                result += chunk;
-            });
-
-            res.on('end', function () {
-                var ranks = [];
-                var regex = /<K>(.+?)<\/K>/g;
-
-                while (regex.test(result)) {
-                    ranks.push(RegExp.$1);
-                }
-
-                callback(ranks);
-            });
-        });
-
-        req.end();
 
     });
 }
