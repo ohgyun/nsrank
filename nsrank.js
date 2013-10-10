9a0775aaf579ecca9b27df0acb33ff85fcb4479e
Ohgyun Ahn <ohgyun@gmail.com>
2013-10-10 15:23:28 +0900

    URL 모듈 도입

--- nsrank.js
+++ nsrank.js
@@ -1,19 +1,18 @@
 var urlReader = require('./urlReader');
 var http = require('http');
+var url = require('url');
 
-urlReader.read(function (url) {
+urlReader.read(function (urlText) {
 
-    console.log(url);
+    var urlData = url.parse(urlText);
 
-    // http.request()로 보낼 수 있지만,
-    // url을 host, path 등으로 나눠서 보내야해요.
     var req = http.request({
-        host: 'www.naver.com',
-        path: '/',
+        host: urlData.host,
+        path: urlData.path,
+        method: 'GET',
         port: 80
     });
 
-    // 응답이 왔을 오면 아래 이벤트가 발생합니다.
     req.on('response', function (res) {
         var result = '';
         res.setEncoding('utf8');
